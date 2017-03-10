07feea158d33f677b91936d270c24863948d5bad
niole <niolenelson@gmail.com>
2017-03-10 13:38:28 -0800

    sets up basic interface for ReviewSessions component

--- app/constants.js
+++ app/constants.js
@@ -1,3 +1,11 @@
+/* main view enum */
 export const VIEW_ANSWERS_VIEW = "viewAnswersView";
 export const CREATE_SESSION_VIEW = "createReviewSessionView";
 export const ANSWER_QUESTIONS_VIEW = "answerQuestionsView";
+
+/* header labels */
+export const PICK_SESSION_LABEL = "Pick a Session";
+export const SESSION_LABEL = "Session";
+export const EDITABLE_QS_LABEL = "Answer Questions";
+export const VIEW_ONLY_QS_LABEL = "View Answers";
+export const REVIEWER_LABEL = "Reviewer";
--- app/view/SiteContainer.jsx
+++ app/view/SiteContainer.jsx
@@ -2,9 +2,14 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import MUIBaseTheme from './MUIBaseTheme.jsx';
 import {
+  REVIEWER_LABEL,
   VIEW_ANSWERS_VIEW,
   CREATE_SESSION_VIEW,
   ANSWER_QUESTIONS_VIEW,
+  PICK_SESSION_LABEL,
+  SESSION_LABEL,
+  EDITABLE_QS_LABEL,
+  VIEW_ONLY_QS_LABEL,
 } from '../constants.js';
 import CreateReviewSession from './CreateReviewSession.jsx';
 import ReviewSessions from './ReviewSessions.jsx';
@@ -12,6 +17,49 @@ import ReviewSessions from './ReviewSessions.jsx';
 
 
 class SiteContainer extends MUIBaseTheme {
+  /**
+   * depending on main view type
+   * sends user to a view with all peers in session
+   * or sends user to view with all peers in session
+   * except self
+   */
+  toSession() {
+  }
+
+  /**
+   * only used when session creator views reviews from peers in
+   * session, which user created
+   */
+  toReviewed() {
+  }
+
+  /**
+   * sends user to Q/A view, where answers are
+   * editable if user is not session creator
+   * and are not editable if user is session creator
+   */
+  toQuestions() {
+  }
+
+  getViewAnswersView() {
+    return (
+      <ReviewSessions
+        type={ VIEW_ANSWERS_VIEW }
+        headers={[
+          PICK_SESSION_LABEL,
+          SESSION_LABEL,
+          REVIEWER_LABEL,
+          VIEW_ONLY_QS_LABEL,
+        ]}
+        elementClickHandlers={[
+          this.toSession,
+          this.toReviewed,
+          this.toQuestions,
+        ]}
+      />
+    );
+  }
+
   getCurrentView() {
     const {
       mainView,
@@ -19,7 +67,7 @@ class SiteContainer extends MUIBaseTheme {
 
     switch (mainView) {
       case VIEW_ANSWERS_VIEW:
-        return <ReviewSessions type={ VIEW_ANSWERS_VIEW }/>;
+        return this.getViewAnswersView();
       case CREATE_SESSION_VIEW:
         return <CreateReviewSession/>;
       case ANSWER_QUESTIONS_VIEW:
